pkgw/casa-python

commit 5a4f258bc9e4096e4f75ede943d9aeea19a910a4

New Development: No             JIRA Issue: Yes CAS-4211, CAS-4213, CAS-4214 and CAS-4216         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: task parameters          Test Programs:   Put in Release Notes: No              Module(s): gcwrap            Description: added new task params: restfreq for sdfit, sdflag and sdreduce. specunit, frame, doppler for sdflag
git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/gcwrap@20479 ac9c0da5-131f-0410-9009-a9c195f183e6

python/scripts/task_sdreduce.py
@@ -5,6 +5,7 @@ import string
 import inspect
 from odict import odict
 
+import sdutil
 import asap as sd
 from asap._asap import Scantable
 import pylab as pl
@@ -12,7 +13,7 @@ from sdcal import sdcal
 from sdsmooth import sdsmooth
 from sdbaseline import sdbaseline
 
-def sdreduce(infile, antenna, fluxunit, telescopeparm, specunit, frame, doppler, calmode, scanlist, field, iflist, pollist, channelrange, average, scanaverage, timeaverage, tweight, averageall, polaverage, pweight, tau, kernel, kwidth, chanwidth, masklist, maskmode, thresh, avg_limit, edge, blfunc, order, npiece, applyfft, fftmethod, fftthresh, addwn, rejwn, clipthresh, clipniter, verifycal, verifysm, verifybl, verbosebl, showprogress, minnrow, outfile, outform, overwrite, plotlevel):
+def sdreduce(infile, antenna, fluxunit, telescopeparm, specunit, restfreq, frame, doppler, calmode, scanlist, field, iflist, pollist, channelrange, average, scanaverage, timeaverage, tweight, averageall, polaverage, pweight, tau, kernel, kwidth, chanwidth, masklist, maskmode, thresh, avg_limit, edge, blfunc, order, npiece, applyfft, fftmethod, fftthresh, addwn, rejwn, clipthresh, clipniter, verifycal, verifysm, verifybl, verbosebl, showprogress, minnrow, outfile, outform, overwrite, plotlevel):
 
         a=inspect.stack()
         stacklevel=0
@@ -134,7 +135,7 @@ def sdreduce(infile, antenna, fluxunit, telescopeparm, specunit, frame, doppler,
             if blfunc != 'none':
               tmpoutfile = sdbaselineout
               #sdbaseline.defaults()
-              sdbaseline(infile=tmpinfile,antenna=antenna,masklist=masklist,maskmode=maskmode,thresh=thresh,avg_limit=avg_limit,edge=edge,blfunc=blfunc,order=order,npiece=npiece,applyfft=applyfft,fftmethod=fftmethod,fftthresh=fftthresh,addwn=addwn,rejwn=rejwn,clipthresh=clipthresh,clipniter=clipniter,verify=verifybl,verbose=verbosebl,showprogress=showprogress,minnrow=minnrow,outfile=tmpoutfile,outform=outform,overwrite=True,plotlevel=plotlevel)
+              sdbaseline(tmpinfile, antenna, fluxunit, telescopeparm, specunit, restfreq, frame, doppler, scanlist, field, iflist, pollist, tau, masklist, maskmode, thresh, avg_limit, edge, blfunc, order, npiece, applyfft, fftmethod, fftthresh, addwn, rejwn, clipthresh, clipniter, verifybl, verbosebl, showprogress, minnrow, tmpoutfile, outform, True, plotlevel)
             else:
               #print "No baseline subtraction was applied..."
               #print ""
@@ -165,7 +166,7 @@ def _reset_inputs(param=None):
         internal function to recover inputs of sdreduce (containing other tasks) with global task parameter settin
 g
         '''
-        arg_names=['infile','antenna','fluxunit','telescopeparm','specunit','frame','doppler','calmode','scanlist','field','iflist','pollist','channelrange','average','scanaverage','timeaverage','tweight','averageall','polaverage','pweight','tau','kernel','kwidth','blfunc','order','npiece','applyfft','fftmethod','fftthresh','addwn','rejwn','clipthresh','clipniter','masklist','maskmode','thresh','avg_limit','edge','verifycal','verifysm','verifybl','verbosebl','showprogress','minnrow','outfile','outform','overwrite','plotlevel']
+        arg_names=['infile','antenna','fluxunit','telescopeparm','specunit','restfreq','frame','doppler','calmode','scanlist','field','iflist','pollist','channelrange','average','scanaverage','timeaverage','tweight','averageall','polaverage','pweight','tau','kernel','kwidth','blfunc','order','npiece','applyfft','fftmethod','fftthresh','addwn','rejwn','clipthresh','clipniter','masklist','maskmode','thresh','avg_limit','edge','verifycal','verifysm','verifybl','verbosebl','showprogress','minnrow','outfile','outform','overwrite','plotlevel']
         a=inspect.stack()
         stacklevel=0
         for k in range(len(a)):

python/scripts/task_sdfit.py
@@ -1,11 +1,12 @@
 import os
 from taskinit import *
 
+import sdutil
 import asap as sd
 import pylab as pl
 from numpy import ma, array, logical_not, logical_and
 
-def sdfit(infile, antenna, fluxunit, telescopeparm, specunit, frame, doppler, scanlist, field, iflist, pollist, fitfunc, fitmode, maskline, invertmask, nfit, thresh, min_nchan, avg_limit, box_size, edge, outfile, overwrite, plotlevel):
+def sdfit(infile, antenna, fluxunit, telescopeparm, specunit, restfreq, frame, doppler, scanlist, field, iflist, pollist, fitfunc, fitmode, maskline, invertmask, nfit, thresh, min_nchan, avg_limit, box_size, edge, outfile, overwrite, plotlevel):
 
 
         casalog.origin('sdfit')
@@ -85,6 +86,17 @@ def sdfit(infile, antenna, fluxunit, telescopeparm, specunit, frame, doppler, sc
 		    s = sorg
 	    del sorg
 	    
+	    # set restfreq
+	    modified_molid = False
+	    if (specunit == 'km/s'):
+		    if (restfreq == '') and (len(s.get_restfreqs()[0]) == 0):
+			    mesg = "Restfreq must be given."
+			    raise Exception, mesg
+		    elif (len(str(restfreq)) > 0):
+			    molids = s._getmolidcol_list()
+			    s.set_restfreqs(sdutil.normalise_restfreq(restfreq))
+			    modified_molid = True
+
             # get telescope name
             #'ATPKSMB', 'ATPKSHOH', 'ATMOPRA', 'DSS-43' (Tid), 'CEDUNA', and 'HOBART'
             antennaname = s.get_antennaname()
@@ -592,6 +604,10 @@ def sdfit(infile, antenna, fluxunit, telescopeparm, specunit, frame, doppler, sc
                     #print fitparams
                     store_fit(fitfunc, outfile, fitparams, s, overwrite)
                 
+	    #restore the original moleculeID column
+	    if modified_molid:
+		    s._setmolidcol_list(molids)
+	    
             # Final clean up
             del f
             del s

python/scripts/task_sdflag.py
@@ -1,12 +1,13 @@
 import os
 from taskinit import *
 
+import sdutil
 import asap as sd
 from asap.flagplotter import flagplotter
 import pylab as pl
 from numpy import ma, array, logical_not, logical_and
 
-def sdflag(infile, antenna, scanlist, field, iflist, pollist, maskflag, flagrow, clip, clipminmax, clipoutside, flagmode, interactive, showflagged, outfile, outform, overwrite, plotlevel):
+def sdflag(infile, antenna, specunit, restfreq, frame, doppler, scanlist, field, iflist, pollist, maskflag, flagrow, clip, clipminmax, clipoutside, flagmode, interactive, showflagged, outfile, outform, overwrite, plotlevel):
 
         casalog.origin('sdflag')
 
@@ -43,15 +44,26 @@ def sdflag(infile, antenna, scanlist, field, iflist, pollist, maskflag, flagrow,
 		    s = sorg
 	    del sorg
 
+	    # set restfreq
+	    modified_molid = False
+	    if (specunit == 'km/s'):
+		    if (restfreq == '') and (len(s.get_restfreqs()[0]) == 0):
+			    mesg = "Restfreq must be given."
+			    raise Exception, mesg
+		    elif (len(str(restfreq)) > 0):
+			    molids = s._getmolidcol_list()
+			    s.set_restfreqs(sdutil.normalise_restfreq(restfreq))
+			    modified_molid = True
+
             #check the format of the infile
             if isinstance(infile, str):
-              if os.path.isdir(filename) and os.path.exists(filename+'/table.info'):
-                if os.path.exists(filename+'/table.f1'):
-                  format = 'MS2'
-                else:
-                  format = 'ASAP'
-              else:
-                  format = 'SDFITS'
+		    if os.path.isdir(filename) and os.path.exists(filename+'/table.info'):
+			    if os.path.exists(filename+'/table.f1'):
+				    format = 'MS2'
+			    else:
+				    format = 'ASAP'
+		    else:
+			    format = 'SDFITS'
 
 	    # Check the formats of infile and outfile are identical when overwrite=True.
 	    # (CAS-3096). If not, print warning message and exit.
@@ -91,10 +103,33 @@ def sdflag(infile, antenna, scanlist, field, iflist, pollist, maskflag, flagrow,
             #Currently only channel based flag is allowed
             # so make sure the input data is channel 
             unit_in=s.get_unit()
-            if unit_in!='channel':
-                s.set_unit('channel')
+            #if unit_in!='channel':
+            #    s.set_unit('channel')
+	    
+	    # set default spectral axis unit
+	    if ( specunit != '' ):
+		    s.set_unit(specunit)
 
-            antennaname = s.get_antennaname()
+	    # reset frame and doppler if needed
+	    if ( frame != '' ):
+		    s.set_freqframe(frame)
+	    else:
+		    casalog.post( 'Using current frequency frame' )
+	    
+	    if ( doppler != '' ):
+		    if ( doppler == 'radio' ):
+			    ddoppler = 'RADIO'
+		    elif ( doppler == 'optical' ):
+			    ddoppler = 'OPTICAL'
+		    elif ( doppler == 'z' ):
+			    ddoppler = 'Z'
+		    else:
+			    ddoppler = doppler
+			
+		    s.set_doppler(ddoppler)
+	    else:
+		    casalog.post( 'Using current doppler convention' )
+
 
             # Select scan and field
             sel = sd.selector()
@@ -382,6 +417,11 @@ def sdflag(infile, antenna, scanlist, field, iflist, pollist, maskflag, flagrow,
             s.set_unit(unit_in) 
             sel.reset()
             s.set_selection(sel)
+
+	    #restore the original moleculeID column
+	    if modified_molid:
+		    s._setmolidcol_list(molids)
+	    
             s.save(spefile,outform,overwrite)
 	    
             if outform!='ASCII':

tasks/sdbaseline.xml
@@ -334,10 +334,10 @@ xsi:schemaLocation="http://casa.nrao.edu/schema/casa.xsd file:///opt/casa/code/x
                                  telescopeparm='FIX' to change default fluxunit
                                  see description below
         specunit -- units for spectral axis
-                options: (str) 'channel','km/s','GHz','MHz','kHz','Hz'
+                options: (str) 'channel','km/s','GHz','MHz','kHz','Hz',''
                 default: '' (=current)
                 example: this will be the units for masklist
-            &gt;&gt;&gt; blfunc expandable parameters
+            &gt;&gt;&gt; specunit expandable parameters
                  restfreq -- rest frequency
                          available type includes float, int, string, list of float, 
                          list of int, list of string, and list of dictionary. the 

tasks/sdfit.xml
@@ -10,14 +10,14 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
 
 <task type="function" name="sdfit" startup="false" category="single dish">
 
-  <shortdescription>ASAP SD task: fit a spectral line</shortdescription>
+<shortdescription>ASAP SD task: fit a spectral line</shortdescription>
 
   <description>
-        ASAP SD line-fitting task:
+    ASAP SD line-fitting task:
 
-        Modified 2008-01-29 TT
-        Task Version 2007-03-04 STM
-</description>
+    Modified 2008-01-29 TT
+    Task Version 2007-03-04 STM
+  </description>
 
   <input>
 
@@ -55,6 +55,12 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
             <value>kHz</value>
             <value>Hz</value>
     </allowed>
+    </param>
+
+    <param type="any" name="restfreq" subparam="true">
+        <description>rest frequency (default unit: Hz)</description>
+        <any type="variant" limittype="double int string doubleArray intArray stringArray dictArray" />
+        <value type="string"></value>
     </param>
 
     <param type="string" name="frame">
@@ -176,6 +182,17 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
                         <default param="telescopeparm"><value type='string'></value></default>
                 </equals>
             </when>
+	    <when param="specunit">
+		<equals value=""/>
+		<equals value="channel"/>
+		<equals value="GHz"/>
+		<equals value="MHz"/>
+		<equals value="kHz"/>
+		<equals value="Hz"/>
+		<equals value="km/s">
+			<default param="restfreq"><value type='string'></value></default>
+                </equals>
+	    </when>
             <when param="fitmode">
                 <equals value="auto">
                         <default param="thresh"><value>5.0</value></default>
@@ -189,18 +206,18 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
             </when>
     </constraints>
 
-    </input>
-    <output>
+  </input>
+  <output>
     <param type="any" name="xstat">
 	    <description>RETURN ONLY: a Python dictionary of line statistics</description>
             <any type='variant'/>
 	    <value>{}</value>
     </param>
-    </output>
+  </output>
 
-  <returns type="void"/>
+<returns type="void"/>
 
-  <example>
+<example>
         Keyword arguments:
         infile -- name of input SD dataset
                 default: none - must input file name
@@ -230,6 +247,26 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
         specunit -- units for spectral axis
                 options: (str) 'channel','km/s','GHz','MHz','kHz','Hz',''
                 default: '' (=current)
+                example: this will be the units for masklist
+            &gt;&gt;&gt; specunit expandable parameters
+                 restfreq -- rest frequency
+                         available type includes float, int, string, list of float, 
+                         list of int, list of string, and list of dictionary. the 
+                         default unit of restfreq in case of float, int, or string 
+                         without unit is Hz. string input can be a value only 
+                         (treated as Hz) or a value followed by unit for which 'GHz',
+                         'MHz','kHz',and 'Hz' are available. 
+                         a list can be used to set different rest frequencies for 
+                         each IF. the length of list input must be nIF. dictionary 
+                         input should be a pair of molecule name and frequency with 
+                         keys of 'name' and 'value', respectively. values in the 
+                         dictionary input follows the same manner as for single 
+                         float or string input. 
+                         example: 345.796
+                                  '1420MHz'
+                                  [345.8, 347.0, 356.7]
+                                  ['345.8MHz', '347.0MHz', '356.7MHz']
+                                  [{'name':'CO','value':345}]
         frame -- frequency frame for spectral axis
                 options: (str) 'LSRK','REST','TOPO','LSRD','BARY',
                          'GEO','GALACTO','LGROUP','CMB'

tasks/sdflag.xml
@@ -32,6 +32,36 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
             <value type="int">0</value>
     </param> 
 
+    <param type="string" name="specunit">
+	<description>units for spectral axis (channel,km/s,GHz)</description>
+	<value></value>
+	<allowed kind="enum">
+	    <value></value>
+	    <value>channel</value>
+	    <value>km/s</value>
+	    <value>GHz</value>
+	    <value>MHz</value>
+	    <value>kHz</value>
+	    <value>Hz</value>
+        </allowed>
+    </param>
+
+    <param type="any" name="restfreq" subparam="true">
+        <description>rest frequency (default unit: Hz)</description>
+        <any type="variant" limittype="double int string doubleArray intArray stringArray dictArray" />
+        <value type="string"></value>
+    </param>
+
+    <param type="string" name="frame">
+	    <description>frequency reference frame, e.g. LSRK (''=current)</description>
+	    <value></value>
+    </param>
+
+    <param type="string" name="doppler">
+	    <description>doppler convention, e.g. RADIO (''=current)</description>
+	    <value></value>
+    </param>
+
     <param type="intArray" name="scanlist">
 	    <description>list of scans to use (e.g. [1,2,3,4])</description>
 	    <value></value>
@@ -129,6 +159,17 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
     </param>
 
     <constraints>
+	    <when param="specunit">
+		<equals value=""/>
+		<equals value="channel"/>
+		<equals value="GHz"/>
+		<equals value="MHz"/>
+		<equals value="kHz"/>
+		<equals value="Hz"/>
+		<equals value="km/s">
+			<default param="restfreq"><value type='string'></value></default>
+                </equals>
+	    </when>
             <when param="clip">
                     <equals type="bool" value="False"/>
                     <equals type="bool" value="True">
@@ -152,6 +193,37 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
         Keyword arguments:
         infile -- name of input SD dataset
         antenna -- antenna name or id (only effective for MS input). 
+        specunit -- units for spectral axis
+                options: (str) 'channel','km/s','GHz','MHz','kHz','Hz',''
+                default: '' (=current)
+                example: this will be the units for maskflag
+            &gt;&gt;&gt; specunit expandable parameters
+                 restfreq -- rest frequency
+                         available type includes float, int, string, list of float, 
+                         list of int, list of string, and list of dictionary. the 
+                         default unit of restfreq in case of float, int, or string 
+                         without unit is Hz. string input can be a value only 
+                         (treated as Hz) or a value followed by unit for which 'GHz',
+                         'MHz','kHz',and 'Hz' are available. 
+                         a list can be used to set different rest frequencies for 
+                         each IF. the length of list input must be nIF. dictionary 
+                         input should be a pair of molecule name and frequency with 
+                         keys of 'name' and 'value', respectively. values in the 
+                         dictionary input follows the same manner as for single 
+                         float or string input. 
+                         example: 345.796
+                                  '1420MHz'
+                                  [345.8, 347.0, 356.7]
+                                  ['345.8MHz', '347.0MHz', '356.7MHz']
+                                  [{'name':'CO','value':345}]
+        frame -- frequency frame for spectral axis
+                options: (str) 'LSRK','REST','TOPO','LSRD','BARY',
+                         'GEO','GALACTO','LGROUP','CMB'
+                default: currently set frame in scantable
+                WARNING: frame='REST' not yet implemented
+        doppler -- doppler mode
+                options: (str) 'RADIO','OPTICAL','Z','BETA','GAMMA'
+                default: currently set doppler in scantable
         scanlist -- list of scan numbers to process
                 default: [] (use all scans)
                 example: [21,22,23,24]

tasks/sdreduce.xml
@@ -58,6 +58,12 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
     </allowed>
     </param>
 
+    <param type="any" name="restfreq" subparam="true">
+        <description>rest frequency (default unit: Hz)</description>
+        <any type="variant" limittype="double int string doubleArray intArray stringArray dictArray" />
+        <value type="string"></value>
+    </param>
+
     <param type="string" name="frame">
 	    <description>frequency reference frame, e.g. LSRK (''=current)</description>
 	    <value></value>
@@ -346,6 +352,17 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
 			<default param="telescopeparm"><value>""</value></default>
 	        </equals>
 	    </when>
+	    <when param="specunit">
+		<equals value=""/>
+		<equals value="channel"/>
+		<equals value="GHz"/>
+		<equals value="MHz"/>
+		<equals value="kHz"/>
+		<equals value="Hz"/>
+		<equals value="km/s">
+			<default param="restfreq"><value type='string'></value></default>
+                </equals>
+	    </when>
             <when param="average">
                 <equals type="bool" value="False"/>
                 <equals type="bool" value="True">
@@ -438,6 +455,25 @@ file:///opt/casa/code/xmlcasa/xml/casa.xsd">
                 options: (str) 'channel','km/s','GHz','MHz','kHz','Hz',''
                 default: '' (=current)
                 example: this will be the units for masklist
+            &gt;&gt;&gt; specunit expandable parameters
+                 restfreq -- rest frequency
+                         available type includes float, int, string, list of float, 
+                         list of int, list of string, and list of dictionary. the 
+                         default unit of restfreq in case of float, int, or string 
+                         without unit is Hz. string input can be a value only 
+                         (treated as Hz) or a value followed by unit for which 'GHz',
+                         'MHz','kHz',and 'Hz' are available. 
+                         a list can be used to set different rest frequencies for 
+                         each IF. the length of list input must be nIF. dictionary 
+                         input should be a pair of molecule name and frequency with 
+                         keys of 'name' and 'value', respectively. values in the 
+                         dictionary input follows the same manner as for single 
+                         float or string input. 
+                         example: 345.796
+                                  '1420MHz'
+                                  [345.8, 347.0, 356.7]
+                                  ['345.8MHz', '347.0MHz', '356.7MHz']
+                                  [{'name':'CO','value':345}]
         frame -- frequency frame for spectral axis
                 options: (str) 'LSRK','REST','TOPO','LSRD','BARY',
                          'GEO','GALACTO','LGROUP','CMB'

commit 3f50d89e7ab606c7d14bd0b4cc9c7c72494b2620

New Development: No             JIRA Issue: No (a bug fix)         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: irc_hc3n_task_regression.py, irc_sio_regression.py   Put in Release Notes: No              Module(s): sdreduce            Description: Fixed a bug introduced in r20479. Data selection, restfreq, and frame settings are done at sdcal (always invoked) in sdreduce. So they should not applied again in sdbaseline. Otherwise, sdbaseline fails due to renumbering of SCANNO, etc. in the previous steps.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/active/gcwrap@20802 ac9c0da5-131f-0410-9009-a9c195f183e6

python/scripts/task_sdreduce.py
@@ -135,7 +135,7 @@ def sdreduce(infile, antenna, fluxunit, telescopeparm, specunit, restfreq, frame
             if blfunc != 'none':
               tmpoutfile = sdbaselineout
               #sdbaseline.defaults()
-              sdbaseline(tmpinfile, antenna, fluxunit, telescopeparm, specunit, restfreq, frame, doppler, scanlist, field, iflist, pollist, tau, masklist, maskmode, thresh, avg_limit, edge, blfunc, order, npiece, applyfft, fftmethod, fftthresh, addwn, rejwn, clipthresh, clipniter, verifybl, verbosebl, showprogress, minnrow, tmpoutfile, outform, True, plotlevel)
+              sdbaseline(infile=tmpinfile, antenna=antenna, masklist=masklist, maskmode=maskmode, thresh=thresh, avg_limit=avg_limit, edge=edge, blfunc=blfunc, order=order, npiece=npiece, applyfft=applyfft, fftmethod=fftmethod, fftthresh=fftthresh, addwn=addwn, rejwn=rejwn, clipthresh=clipthresh, clipniter=clipniter, verify=verifybl, verbose=verbosebl, showprogress=showprogress, minnrow=minnrow, outfile=tmpoutfile, outform=outform, overwrite=True, plotlevel=plotlevel)
             else:
               #print "No baseline subtraction was applied..."
               #print ""